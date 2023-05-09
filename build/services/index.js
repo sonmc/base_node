690c73e43db53b2e65e0b19bca109f39f1527bdb
SonMc <maicongson@zen8labs.com>
2023-05-09 16:09:15 +0700

    remove node module and build

--- build/services/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.services = void 0;
-const user_service_1 = require("./user/user.service");
-exports.services = [user_service_1.UserService];
--- build/use-case/user/user.ctrl.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAll = void 0;
-function getAll(ctx, _next) {
-    ctx.body = 'Hello world';
-}
-exports.getAll = getAll;
-exports.default = { getAll };
